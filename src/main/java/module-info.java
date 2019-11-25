4210cda8c52d785f992e3d03f53d770c14d80723
Kim Christian Gaarder <kim.christian.gaarder@gmail.com>
2019-11-25 15:50:45 +0100

    Make all runtime (not visible at compile-time) dependencies explicit

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -9,10 +9,19 @@ module no.ssb.datasetaccess {
     requires org.slf4j;
     requires logstash.logback.encoder;
     requires reactive.pg.client;
-    requires java.sql; // required by micronaut-metrics
-    requires jdk.unsupported; // required by netty to allow reliable low-level API access to direct-buffers
     requires com.fasterxml.jackson.databind;
     requires com.fasterxml.jackson.annotation;
+
+    /*
+     * Not so well documented requirements are declared here to force fail-fast with proper error message if
+     * missing from jvm.
+     */
+    requires java.sql; // required by micronaut-metrics
+    requires java.desktop; // required by micronaut inject for bean introspection
+    requires jdk.zipfs; // required by micronaut for classpath scanning
+    requires jdk.management.agent; // required to run application with the com.sun.management.jmxremote property
+    requires jdk.unsupported; // required by netty to allow reliable low-level API access to direct-buffers
+    requires jdk.naming.dns; // required by netty dns libraries used by reactive postgres
 
     opens no.ssb.datasetaccess;
 
