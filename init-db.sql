e588cbb16adfeddb010199dbdd0464745ada295e
Øyvind Strømmen <insula.ventus@gmail.com>
2019-12-04 10:21:46 +0100

    Add an initial flyway migration script

--- init-db.sql
+++ init-db.sql
@@ -4,28 +4,3 @@
 CREATE USER rdc WITH PASSWORD 'rdc';
 CREATE DATABASE rdc;
 GRANT ALL PRIVILEGES ON DATABASE rdc TO rdc;
-
-
--- Connect to database 'rdc' with user 'rdc'
-\connect rdc rdc
-
--- Create dataset table
-CREATE TABLE dataset
-(
-    datasetId varchar(100) PRIMARY KEY,
-    document  jsonb
-);
-
--- Create role table
-CREATE TABLE role
-(
-    roleId   varchar(100) PRIMARY KEY,
-    document jsonb
-);
-
--- Create user table
-CREATE TABLE user_permission
-(
-    userId   varchar(100) PRIMARY KEY,
-    document jsonb
-);
