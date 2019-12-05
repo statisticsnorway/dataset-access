94e68bcff0af1961b0e408cc67a6ec26669217f6
Kim Christian Gaarder <kim.christian.gaarder@gmail.com>
2019-12-05 14:27:40 +0100

    Use method references instead of lambdas for routing handlers

--- src/main/java/no/ssb/datasetaccess/role/RoleService.java
+++ src/main/java/no/ssb/datasetaccess/role/RoleService.java
@@ -4,6 +4,8 @@ package no.ssb.datasetaccess.role;
 import io.helidon.common.http.Http;
 import io.helidon.webserver.Handler;
 import io.helidon.webserver.Routing;
+import io.helidon.webserver.ServerRequest;
+import io.helidon.webserver.ServerResponse;
 import io.helidon.webserver.Service;
 
 public class RoleService implements Service {
@@ -16,52 +18,46 @@ public class RoleService implements Service {
 
     @Override
     public void update(Routing.Rules rules) {
-        rules.get("/{roleId}", getRoleHandler());
-        rules.put("/{roleId}", putRoleHandler());
-        rules.delete("/{roleId}", deleteRoleHandler());
+        rules.get("/{roleId}", this::doGet);
+        rules.put("/{roleId}", Handler.create(Role.class, this::doPut));
+        rules.delete("/{roleId}", this::doDelete);
     }
 
-    private Handler getRoleHandler() {
-        return (req, res) -> {
-            String roleId = req.path().param("roleId");
-            repository.getRole(roleId)
-                    .thenAccept(role -> {
-                        if (role == null) {
-                            res.status(Http.Status.NOT_FOUND_404).send();
-                        } else {
-                            res.send(role);
-                        }
-                    });
-        };
+    private void doGet(ServerRequest req, ServerResponse res) {
+        String roleId = req.path().param("roleId");
+        repository.getRole(roleId)
+                .thenAccept(role -> {
+                    if (role == null) {
+                        res.status(Http.Status.NOT_FOUND_404).send();
+                    } else {
+                        res.send(role);
+                    }
+                });
     }
 
-    private Handler putRoleHandler() {
-        return Handler.create(Role.class, (req, res, role) -> {
-            String roleId = req.path().param("roleId");
-            if (!roleId.equals(role.getRoleId())) {
-                res.status(Http.Status.BAD_REQUEST_400).send("roleId in path must match that in body");
-            }
-            repository.createRole(role)
-                    .thenRun(() -> {
-                        res.headers().add("Location", "/role/" + roleId);
-                        res.status(Http.Status.CREATED_201).send();
-                    })
-                    .exceptionally(t -> {
-                        res.status(Http.Status.INTERNAL_SERVER_ERROR_500).send(t.getMessage());
-                        return null;
-                    });
-        });
+    private void doPut(ServerRequest req, ServerResponse res, Role role) {
+        String roleId = req.path().param("roleId");
+        if (!roleId.equals(role.getRoleId())) {
+            res.status(Http.Status.BAD_REQUEST_400).send("roleId in path must match that in body");
+        }
+        repository.createRole(role)
+                .thenRun(() -> {
+                    res.headers().add("Location", "/role/" + roleId);
+                    res.status(Http.Status.CREATED_201).send();
+                })
+                .exceptionally(t -> {
+                    res.status(Http.Status.INTERNAL_SERVER_ERROR_500).send(t.getMessage());
+                    return null;
+                });
     }
 
-    private Handler deleteRoleHandler() {
-        return (req, res) -> {
-            String roleId = req.path().param("roleId");
-            repository.deleteRole(roleId)
-                    .thenRun(res::send)
-                    .exceptionally(t -> {
-                        res.status(Http.Status.INTERNAL_SERVER_ERROR_500).send(t.getMessage());
-                        return null;
-                    });
-        };
+    private void doDelete(ServerRequest req, ServerResponse res) {
+        String roleId = req.path().param("roleId");
+        repository.deleteRole(roleId)
+                .thenRun(res::send)
+                .exceptionally(t -> {
+                    res.status(Http.Status.INTERNAL_SERVER_ERROR_500).send(t.getMessage());
+                    return null;
+                });
     }
 }
--- src/main/java/no/ssb/datasetaccess/user/UserService.java
+++ src/main/java/no/ssb/datasetaccess/user/UserService.java
@@ -3,6 +3,8 @@ package no.ssb.datasetaccess.user;
 import io.helidon.common.http.Http;
 import io.helidon.webserver.Handler;
 import io.helidon.webserver.Routing;
+import io.helidon.webserver.ServerRequest;
+import io.helidon.webserver.ServerResponse;
 import io.helidon.webserver.Service;
 
 public class UserService implements Service {
@@ -15,52 +17,46 @@ public class UserService implements Service {
 
     @Override
     public void update(Routing.Rules rules) {
-        rules.get("/{userId}", getUserHandler());
-        rules.put("/{userId}", putUserHandler());
-        rules.delete("/{userId}", deleteUserHandler());
+        rules.get("/{userId}", this::doGet);
+        rules.put("/{userId}", Handler.create(User.class, this::doPut));
+        rules.delete("/{userId}", this::doDelete);
     }
 
-    private Handler getUserHandler() {
-        return (req, res) -> {
-            String userId = req.path().param("userId");
-            repository.getUser(userId)
-                    .thenAccept(user -> {
-                        if (user == null) {
-                            res.status(Http.Status.NOT_FOUND_404).send();
-                        } else {
-                            res.send(user);
-                        }
-                    });
-        };
+    private void doGet(ServerRequest req, ServerResponse res) {
+        String userId = req.path().param("userId");
+        repository.getUser(userId)
+                .thenAccept(user -> {
+                    if (user == null) {
+                        res.status(Http.Status.NOT_FOUND_404).send();
+                    } else {
+                        res.send(user);
+                    }
+                });
     }
 
-    private Handler putUserHandler() {
-        return Handler.create(User.class, (req, res, user) -> {
-            String userId = req.path().param("userId");
-            if (!userId.equals(user.getUserId())) {
-                res.status(Http.Status.BAD_REQUEST_400).send("userId in path must match that in body");
-            }
-            repository.createUser(user)
-                    .thenRun(() -> {
-                        res.headers().add("Location", "/user/" + userId);
-                        res.status(Http.Status.CREATED_201).send();
-                    })
-                    .exceptionally(t -> {
-                        res.status(Http.Status.INTERNAL_SERVER_ERROR_500).send(t.getMessage());
-                        return null;
-                    });
-        });
+    private void doPut(ServerRequest req, ServerResponse res, User user) {
+        String userId = req.path().param("userId");
+        if (!userId.equals(user.getUserId())) {
+            res.status(Http.Status.BAD_REQUEST_400).send("userId in path must match that in body");
+        }
+        repository.createUser(user)
+                .thenRun(() -> {
+                    res.headers().add("Location", "/user/" + userId);
+                    res.status(Http.Status.CREATED_201).send();
+                })
+                .exceptionally(t -> {
+                    res.status(Http.Status.INTERNAL_SERVER_ERROR_500).send(t.getMessage());
+                    return null;
+                });
     }
 
-    private Handler deleteUserHandler() {
-        return (req, res) -> {
-            String userId = req.path().param("userId");
-            repository.deleteUser(userId)
-                    .thenRun(res::send)
-                    .exceptionally(t -> {
-                        res.status(Http.Status.INTERNAL_SERVER_ERROR_500).send(t.getMessage());
-                        return null;
-                    });
-        };
+    private void doDelete(ServerRequest req, ServerResponse res) {
+        String userId = req.path().param("userId");
+        repository.deleteUser(userId)
+                .thenRun(res::send)
+                .exceptionally(t -> {
+                    res.status(Http.Status.INTERNAL_SERVER_ERROR_500).send(t.getMessage());
+                    return null;
+                });
     }
 }
