f8accae0147496fef8f2c266c149cac59d0933dc
Kim Christian Gaarder <kim.christian.gaarder@gmail.com>
2019-12-13 11:01:53 +0100

    Added protobuf

---
+++ protobuf/src/main/java-mr/11/module-info.java
@@ -0,0 +1,10 @@
+module no.ssb.dapla.auth.dataset.protobuf {
+    requires com.google.common;
+    requires io.grpc;
+    requires grpc.stub;
+    requires protobuf.java;
+    requires grpc.protobuf;
+    requires java.annotation;
+
+    exports no.ssb.dapla.auth.dataset.protobuf;
+}
