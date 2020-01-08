081cd99e25cf41ed677ee02f4aecfde9cf85a04d
Kim Christian Gaarder <kim.christian.gaarder@gmail.com>
2020-01-08 07:59:49 +0100

    All APIs must have timeout and exception handling

--- service/src/main/java/no/ssb/datasetaccess/role/RoleService.java
+++ service/src/main/java/no/ssb/datasetaccess/role/RoleService.java
@@ -8,6 +8,8 @@ import io.helidon.webserver.ServerRequest;
 import io.helidon.webserver.ServerResponse;
 import io.helidon.webserver.Service;
 
+import java.util.concurrent.TimeUnit;
+
 public class RoleService implements Service {
 
     final RoleRepository repository;
@@ -26,12 +28,17 @@ public class RoleService implements Service {
     private void doGet(ServerRequest req, ServerResponse res) {
         String roleId = req.path().param("roleId");
         repository.getRole(roleId)
+                .orTimeout(30, TimeUnit.SECONDS)
                 .thenAccept(role -> {
                     if (role == null) {
                         res.status(Http.Status.NOT_FOUND_404).send();
                     } else {
                         res.send(role);
                     }
+                })
+                .exceptionally(t -> {
+                    res.status(Http.Status.INTERNAL_SERVER_ERROR_500).send(t.getMessage());
+                    return null;
                 });
     }
 
@@ -41,6 +48,7 @@ public class RoleService implements Service {
             res.status(Http.Status.BAD_REQUEST_400).send("roleId in path must match that in body");
         }
         repository.createRole(role)
+                .orTimeout(30, TimeUnit.SECONDS)
                 .thenRun(() -> {
                     res.headers().add("Location", "/role/" + roleId);
                     res.status(Http.Status.CREATED_201).send();
@@ -54,6 +62,7 @@ public class RoleService implements Service {
     private void doDelete(ServerRequest req, ServerResponse res) {
         String roleId = req.path().param("roleId");
         repository.deleteRole(roleId)
+                .orTimeout(30, TimeUnit.SECONDS)
                 .thenRun(res::send)
                 .exceptionally(t -> {
                     res.status(Http.Status.INTERNAL_SERVER_ERROR_500).send(t.getMessage());
